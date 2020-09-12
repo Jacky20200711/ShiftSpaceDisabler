35b9b2b63ba54e0734ff46ee0d9c7bbfb78a7e64
Jacky20200711 <fewer135+1@gmail.com>
2020-09-12 11:11:59 +0800

    App for disable Shift+Space

--- ShiftSpaceDisabler.cpp
+++ ShiftSpaceDisabler.cpp
@@ -4,8 +4,8 @@
 #define HOTKEY_ID 0x53C8
 
 /*
-	KeyCode of HotKey at below url  
-	https://blog.xuite.net/jack.b6539/vb/12834758-%E9%8D%B5%E7%9B%A4keycode%E5%B0%8D%E6%87%89%E8%A1%A8
+    KeyCode of HotKey at below url  
+    https://blog.xuite.net/jack.b6539/vb/12834758-%E9%8D%B5%E7%9B%A4keycode%E5%B0%8D%E6%87%89%E8%A1%A8
 */
 
 using std::cout;
@@ -13,33 +13,33 @@ using std::endl;
 
 int main()
 {
-	
-	// hide the window when this app is running
-	HWND hWnd = GetConsoleWindow();
-	ShowWindow( hWnd, SW_HIDE );
-	
-	// register 'alt + 5' key press
-	if (RegisterHotKey(NULL, HOTKEY_ID, MOD_SHIFT, 0x20))
-	{
-		cout << "Successfully registered SHIFT + SPACE global hotkey" << endl;
-	}
+    
+    // hide the window when this app is running
+    HWND hWnd = GetConsoleWindow();
+    ShowWindow( hWnd, SW_HIDE );
+    
+    // register 'Shift + Space' key press
+    if (RegisterHotKey(NULL, HOTKEY_ID, MOD_SHIFT, 0x20))
+    {
+        cout << "Successfully registered Shift + Space global hotkey" << endl;
+    }
 
-	MSG winMessage = { 0 };
+    MSG winMessage = { 0 };
 
-	// perpetually wait until a message is received
-	while (GetMessage(&winMessage, NULL, 0, 0))
-	{
-		if (winMessage.message == WM_HOTKEY)
-		{
-			cout << "Received hotkey message" << endl;
-		}
-	}
+    // perpetually wait until a message is received
+    while (GetMessage(&winMessage, NULL, 0, 0))
+    {
+        if (winMessage.message == WM_HOTKEY)
+        {
+            cout << "Received hotkey message" << endl;
+        }
+    }
 
-	// unregister the hotkey
-	if (UnregisterHotKey(NULL, HOTKEY_ID))
-	{
-		cout << "Successfully unregistered hotkey" << endl;
-	}
+    // unregister the hotkey
+    if (UnregisterHotKey(NULL, HOTKEY_ID))
+    {
+        cout << "Successfully unregistered hotkey" << endl;
+    }
 
-	return 0;
+    return 0;
 }
